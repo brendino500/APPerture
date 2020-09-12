88082693fa09732c687d8b3a0740e1846980a1eb
brendino500 <brenda.ty@live.com>
2020-09-12 16:20:46 +0100

    update register form

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { registerUser } from '../../lib/api'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles, createMuiTheme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
-import { registerUser } from '../../lib/api'
+import { Container,  CssBaseline, Avatar, Typography } from '@material-ui/core'
+import LockIcon from '@material-ui/icons/Lock'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -81,129 +83,141 @@ function Register() {
   }
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-              onChange={handleChange}
-              value={state.username}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="first_name"
-              label="First Name"
-              name="first_name"
-              autoComplete="first_name"
-              autoFocus
-              onChange={handleChange}
-              value={state.first_name}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="last"
-              label="Last Name"
-              name="last_name"
-              autoComplete="last_name"
-              autoFocus
-              onChange={handleChange}
-              value={state.last_name}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={handleChange}
-              value={state.email}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="Password"
-              name="password"
-              autoComplete="password"
-              type="password"
-              autoFocus
-              onChange={handleChange}
-              value={state.password}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="password_confirmation"
-              label="Password Confirmation"
-              name="password_confirmation"
-              autoComplete="password_confirmation"
-              type="password"
-              autoFocus
-              onChange={handleChange}
-              value={state.password_confirmation}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <input
-              accept="image/*"
-              multiple
-              type="file"
-              required
-              id="outlined-required"
-              label="profile_image"
-              autoComplete="profile-image"
-              variant="outlined"
-              style={{ margin: 8 }}
-              fullWidth
-              margin="normal"
-              name="profile_image"
-              onChange={handleChange}
-              value={state.profile_image}
-            />
-          </Grid>
-          <Button 
-            className={classes.buttonStyle}
-            type="submit"
-            variant="outlined"
-          >
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockIcon />
+        </Avatar>
+        <Typography component="h1" varient="h5">
+          Register
+        </Typography>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                  onChange={handleChange}
+                  value={state.username}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="first_name"
+                  label="First Name"
+                  name="first_name"
+                  autoComplete="first_name"
+                  autoFocus
+                  onChange={handleChange}
+                  value={state.first_name}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="last"
+                  label="Last Name"
+                  name="last_name"
+                  autoComplete="last_name"
+                  autoFocus
+                  onChange={handleChange}
+                  value={state.last_name}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={handleChange}
+                  value={state.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  name="password"
+                  autoComplete="password"
+                  type="password"
+                  autoFocus
+                  onChange={handleChange}
+                  value={state.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="password_confirmation"
+                  label="Password Confirmation"
+                  name="password_confirmation"
+                  autoComplete="password_confirmation"
+                  type="password"
+                  autoFocus
+                  onChange={handleChange}
+                  value={state.password_confirmation}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <input
+                  accept="image/*"
+                  multiple
+                  type="file"
+                  required
+                  id="outlined-required"
+                  label="profile_image"
+                  autoComplete="profile-image"
+                  variant="outlined"
+                  style={{ margin: 8 }}
+                  fullWidth
+                  margin="normal"
+                  name="profile_image"
+                  onChange={handleChange}
+                  value={state.profile_image}
+                />
+              </Grid>
+              <Button 
+                className={classes.buttonStyle}
+                type="submit"
+                variant="outlined"
+              >
             Submit
-          </Button>
-        </form>
-      </Grid>
-    </div>
+              </Button>
+            </form>
+          </Grid>
+        </div>
+      </div>
+    </Container>
+    
   )
 }
 
